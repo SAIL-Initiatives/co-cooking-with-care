606049ee0112c0719c11e997ce78b3e1e3676829
Design4Good <r4g.neiss@gmail.com>
2026-01-01 23:50:36 -0800

    Update home.py

--- src/home.py
+++ src/home.py
@@ -151,6 +151,7 @@ if choice == "Signup":
             # Insert into Users table (UUID-based RLS)
             supabase.table("userprofiles").insert({
                 "id": user.id,
+                "email": email.id,
                 "display_name": display_name
             }).execute()
             st.success("Signup successful! Please log in.")
