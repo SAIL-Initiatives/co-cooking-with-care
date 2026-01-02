441d2fe3055b11e8ecbbfc11dc3f2b9abafe9e8a
Design4Good <r4g.neiss@gmail.com>
2026-01-01 23:47:24 -0800

    Update home.py

--- src/home.py
+++ src/home.py
@@ -24,35 +24,36 @@ if "user" not in st.session_state:
 
 tabs= st.tabs(['Share', 'Users'] )
 
-'''
-id='44da6b55-f712-4cd1-8d18-b49beeb0457b' 
-app_metadata={'provider': 'email', 'providers': ['email']} 
-user_metadata={'email': '1470pancake@gmail.com', 'email_verified': False, 
-'phone_verified': False, 'sub': '44da6b55-f712-4cd1-8d18-b49beeb0457b'}
-
-aud='authenticated' 
-confirmation_sent_at=datetime.datetime(2026, 1, 2, 7, 26, 23, 473154, tzinfo=TzInfo(0)) 
-recovery_sent_at=None email_change_sent_at=None new_email=None new_phone=None 
-invited_at=None action_link=None email='1470pancake@gmail.com' phone='' 
-
-created_at=datetime.datetime(2026, 1, 2, 7, 26, 23, 458624, tzinfo=TzInfo(0)) 
-confirmed_at=None email_confirmed_at=None phone_confirmed_at=None last_sign_in_at=None 
-role='authenticated' updated_at=datetime.datetime(2026, 1, 2, 7, 26, 24, 634023, tzinfo=TzInfo(0)) 
-identities=[UserIdentity(id='44da6b55-f712-4cd1-8d18-b49beeb0457b', 
-identity_id='e6cf7f9d-e6db-4db5-9c19-ba558a28f93d', 
-user_id='44da6b55-f712-4cd1-8d18-b49beeb0457b',
-
-identity_data={'email': '1470pancake@gmail.com', 'email_verified': False, 
-'phone_verified': False, 'sub': '44da6b55-f712-4cd1-8d18-b49beeb0457b'}, 
-provider='email', created_at=datetime.datetime(2026, 1, 2, 7, 26, 23, 469418, tzinfo=TzInfo(0)), 
-last_sign_in_at=datetime.datetime(2026, 1, 2, 7, 26, 23, 469366, tzinfo=TzInfo(0)), 
-updated_at=datetime.datetime(2026, 1, 2, 7, 26, 23, 469418, tzinfo=TzInfo(0)))] 
-is_anonymous=False 
-factors=None
-
-
-44da6b55-f712-4cd1-8d18-b49beeb0457b
-'''
+if 0:
+    '''
+    id='44da6b55-f712-4cd1-8d18-b49beeb0457b' 
+    app_metadata={'provider': 'email', 'providers': ['email']} 
+    user_metadata={'email': '1470pancake@gmail.com', 'email_verified': False, 
+    'phone_verified': False, 'sub': '44da6b55-f712-4cd1-8d18-b49beeb0457b'}
+    
+    aud='authenticated' 
+    confirmation_sent_at=datetime.datetime(2026, 1, 2, 7, 26, 23, 473154, tzinfo=TzInfo(0)) 
+    recovery_sent_at=None email_change_sent_at=None new_email=None new_phone=None 
+    invited_at=None action_link=None email='1470pancake@gmail.com' phone='' 
+    
+    created_at=datetime.datetime(2026, 1, 2, 7, 26, 23, 458624, tzinfo=TzInfo(0)) 
+    confirmed_at=None email_confirmed_at=None phone_confirmed_at=None last_sign_in_at=None 
+    role='authenticated' updated_at=datetime.datetime(2026, 1, 2, 7, 26, 24, 634023, tzinfo=TzInfo(0)) 
+    identities=[UserIdentity(id='44da6b55-f712-4cd1-8d18-b49beeb0457b', 
+    identity_id='e6cf7f9d-e6db-4db5-9c19-ba558a28f93d', 
+    user_id='44da6b55-f712-4cd1-8d18-b49beeb0457b',
+    
+    identity_data={'email': '1470pancake@gmail.com', 'email_verified': False, 
+    'phone_verified': False, 'sub': '44da6b55-f712-4cd1-8d18-b49beeb0457b'}, 
+    provider='email', created_at=datetime.datetime(2026, 1, 2, 7, 26, 23, 469418, tzinfo=TzInfo(0)), 
+    last_sign_in_at=datetime.datetime(2026, 1, 2, 7, 26, 23, 469366, tzinfo=TzInfo(0)), 
+    updated_at=datetime.datetime(2026, 1, 2, 7, 26, 23, 469418, tzinfo=TzInfo(0)))] 
+    is_anonymous=False 
+    factors=None
+    
+    
+    44da6b55-f712-4cd1-8d18-b49beeb0457b
+    '''
 
 
 with tabs[1]:
