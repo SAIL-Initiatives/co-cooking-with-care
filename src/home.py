b43b2f6160e260b44f18fb96232fa02028edf00b
Design4Good <r4g.neiss@gmail.com>
2026-05-03 15:28:40 -0700

    Update home.py

--- src/home.py
+++ src/home.py
@@ -77,7 +77,7 @@ SUPABASE_KEY = os.environ['SUPABASE_KEY']  # YOUR_ANON_KEY
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 st.title("Kullaj is ready!")
-st.markdown('Welcome to the <i>Kullaj table</i>, a virtual space where we share recipes, stories, and heritage')
+st.html('<p>Welcome to the <i>Kullaj table</i>, a virtual space where we share recipes, stories, and heritage.</p>')
 
 
 tabs= st.tabs(['Share', 'Signup to join/host a cooking event', 'System history', 'Terms of services' ] )
